d78f71370cc3a06c399a73a754d858bd804fff11
Connor O'Brien <cobrien@four51.com>
2016-11-20 13:03:40 -0600

    Couldn't find branch, trying with quotes around branch variable

--- scripts/CloneSDKRepos.ps1
+++ scripts/CloneSDKRepos.ps1
@@ -25,5 +25,5 @@ $languages = @(
 git clone -q https://github.com/swagger-api/swagger-codegen C:/projects/swagger-codegen
 
 foreach($language in $languages) {
-    git clone -q --branch=$branch $gitUrl/OrderCloud-$language-SDK.git $output\OrderCloud-$language-SDK
+    git clone -q --branch="$branch" $gitUrl/OrderCloud-$language-SDK.git $output\OrderCloud-$language-SDK
 }
